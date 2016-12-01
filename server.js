7f353a9ce4e2875d69276563946dd6f39078af93
Tomas Jaratz <tomyjara@gmail.com>
2016-12-01 15:06:48 -0300

    3

--- server.js
+++ server.js
@@ -3,10 +3,16 @@ var http = require("http");
 var server = http.createServer();
 server.on("request", function(req, res){
 
-  var date = new Date();
-  var current_hour = date.getHours();
-  var current_min = date.getMinutes();
-  res.end(JSON.stringify(current_hour));
+  var filePath = false;
+  if(req.url == '/'){
+    filePath = "public/index.html";
+
+  }else{
+    filePath = "public" + req.url;
+  }
+
+  var absPath = "./" + filePath;
+  serverWorking(res. absPath);
 
 
 });
